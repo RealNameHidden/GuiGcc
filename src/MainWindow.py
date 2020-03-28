0715062a6d16254fd22926da550108074ed6d2da
RealNameHidden <dheeraj.as008@gmail.com>
2020-03-28 01:42:57 -0400

    communicating with gcc

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,5 +1,6 @@
 from util.util import Util
 import tkinter as tk
+import subprocess
 
 class MainWindow:
     def __init__(self):
@@ -16,8 +17,20 @@ class MainWindow:
         """
         util.center_window(width_of_window,height_of_window,root)
         frame1 = tk.Frame(root, bg="#AEB6BF")
-        frame1.place(relx=0.01, rely=0.01, relwidth=0.98, relheight=1)
+        frame1.place(relx=0.01, rely=0.1, relwidth=0.98, relheight=0.1)
 
+        #The out put widget
+        frame2 = tk.Frame(root)
+        frame2.place(relx=0.01, rely=0.5, relwidth=0.98, relheight=0.5)
+        S = tk.Scrollbar(frame2)
+        T = tk.Text(frame2, height=4, width=100, background="black", foreground="white", font="cambria")
+        S.pack(side=tk.RIGHT, fill=tk.Y)
+        T.pack(side=tk.LEFT, fill=tk.Y)
+        S.config(command=T.yview)
+        T.config(yscrollcommand=S.set)
+        quote = subprocess.run(['C:\\MinGW\\bin\\gcc.exe','gcc','--version'], shell="true", capture_output="true")
+        print(quote)
+        T.insert(tk.END, quote.stdout.decode())
         btn_compile = tk.Button(frame1, text="Compile", width=6)
         btn_compile.pack()
 

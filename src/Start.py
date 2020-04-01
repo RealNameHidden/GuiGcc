8d597b7921ef8f021453861144d1ea854f0b75a1
Dheeraj <dheeraj.as008@gmail.com>
2020-04-01 16:40:09 -0400

    Fixed switch mode option

--- src/Start.py
+++ src/Start.py
@@ -4,36 +4,37 @@ import tkinter as tk
 
 class Start:
     util = Util()
-    root = tk.Tk()
-    def makeWindow(self,event):
+
+    def makeWindow(self,root):
         print("Hi")
-        self.root.destroy()
+        root.destroy()
         main = MainWindow()
 
 
     def __init__(self):
+            root = tk.Tk()
 
-            self.root.title("Smart Gcc GUI")
+            root.title("Smart Gcc GUI")
 
             width_of_window=300
             height_of_window=400
-            canvas = tk.Canvas(self.root,width=width_of_window,height=height_of_window)
+            canvas = tk.Canvas(root,width=width_of_window,height=height_of_window)
             canvas.pack()
             """
             Setting the window to the middle of the screen
             """
-            self.util.center_window(width_of_window,height_of_window,self.root)
-            frame0 = tk.Frame(self.root, bg="#8C8C8C")
+            self.util.center_window(width_of_window,height_of_window,root)
+            frame0 = tk.Frame(root, bg="#8C8C8C")
             frame0.place(relx=0, rely=0, relwidth=1, relheight=1)
-            frame1 = tk.Frame(self.root , bg="#AEB6BF")
+            frame1 = tk.Frame(root , bg="#AEB6BF")
             frame1.place(relx=0.01,rely=0.01,relwidth=0.99,relheight=0.99)
 
-            btn_beginner = tk.Button(frame1, text="Beginner", width=6)
+            btn_beginner = tk.Button(frame1, text="Beginner", width=6, command= lambda: self.makeWindow(root))
             btn_beginner.pack()
             btn_medium = tk.Button(frame1, text="Medium", width=6)
             btn_medium.pack()
             btn_expert = tk.Button(frame1, text="Expert", width=6)
             btn_expert.pack()
 
-            btn_beginner.bind('<Button-1>', self.makeWindow)
-            self.root.mainloop()
+            # btn_beginner.bind('<Button-1>', self.makeWindow)
+            root.mainloop()

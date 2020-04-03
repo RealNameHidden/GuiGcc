180458f232871a80da3208319a9596186dc01714
RealNameHidden <dheeraj.as008@gmail.com>
2020-04-03 19:56:49 -0400

    made start screen pretty

--- src/Start.py
+++ src/Start.py
@@ -24,7 +24,7 @@ class Start:
             root.title("Smart Gcc GUI")
 
             width_of_window=300
-            height_of_window=400
+            height_of_window=300
             canvas = tk.Canvas(root,width=width_of_window,height=height_of_window)
             canvas.pack()
             """
@@ -33,15 +33,18 @@ class Start:
             self.util.center_window(width_of_window,height_of_window,root)
             frame0 = tk.Frame(root, bg="#8C8C8C")
             frame0.place(relx=0, rely=0, relwidth=1, relheight=1)
-            frame1 = tk.Frame(root , bg="#AEB6BF")
-            frame1.place(relx=0.01,rely=0.01,relwidth=0.99,relheight=0.99)
+            frame1 = tk.Frame(frame0, bg="#8C8C8C", bd=3)
+            frame1.place(relx=0.5, rely=0.2, relwidth=0.5, relheight=0.6, anchor='n')
 
-            btn_beginner = tk.Button(frame1, text="Beginner", width=6, command= lambda: self.makeWindow(root,btn_beginner.cget('text')))
-            btn_beginner.pack()
-            btn_medium = tk.Button(frame1, text="Medium", width=6,  command= lambda: self.makeWindow(root,btn_medium.cget('text')))
-            btn_medium.pack()
-            btn_expert = tk.Button(frame1, text="Expert", width=6,  command= lambda: self.makeWindow(root,btn_expert.cget('text')))
-            btn_expert.pack()
+            lbl_user = tk.Label(frame0, text="Select your user type", font="TimesNewRoman", bg="#8C8C8C",bd=5, padx=4)
+            lbl_user.place(relx=0.24, rely=0.04)
+
+            btn_beginner = tk.Button(frame1, text="Beginner", width=6, bg="#EFEEEA", activebackground="#BCE27F", bd=1, command= lambda: self.makeWindow(root,btn_beginner.cget('text')))
+            btn_beginner.place(relx=0.5, rely=0.01, relwidth=0.98, relheight=0.2, anchor='n')
+            btn_medium = tk.Button(frame1, text="Medium", width=6, bg="#EFEEEA", activebackground="#BCE27F", bd=1, command= lambda: self.makeWindow(root,btn_medium.cget('text')))
+            btn_medium.place(relx=0.5, rely=0.25,  relwidth=0.98, relheight=0.2, anchor='n')
+            btn_expert = tk.Button(frame1, text="Expert", width=6, bg="#EFEEEA", activebackground="#BCE27F", bd=1, command= lambda: self.makeWindow(root,btn_expert.cget('text')))
+            btn_expert.place(relx=0.5, rely=0.5,relwidth=0.98, relheight=0.2, anchor='n')
 
             # btn_beginner.bind('<Button-1>', self.makeWindow)
             root.mainloop()
